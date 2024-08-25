b09e6c10469741c15ee8dbeee4b7ce2ff6c993f5
RuanSR <ruansilvarosa@gmail.com>
2024-08-25 13:02:54 -0300

    feat: Adicionou modo editor hint para cena crate

--- src/Modules/@World/Objects/Crate/crate.gd
+++ src/Modules/@World/Objects/Crate/crate.gd
@@ -1,7 +1,16 @@
+tool
 extends Node2D
 
 export var path_skins: String
-export var selected_skin_index: int = 0
+export var selected_skin_index: int = 0 setget set_selected_skin_index
 
 func _ready():
 	$CrateSprite.set_texture($SkinManager.get_sprite_texture())
+	pass
+
+func set_selected_skin_index(value):
+	selected_skin_index = value
+	$CrateSprite.set_texture($SkinManager.get_sprite_texture())
+	
+	if (Engine.is_editor_hint()):
+		update()
